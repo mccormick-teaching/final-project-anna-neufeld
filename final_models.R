d885c9b27e99ab5380038e97b3947c3841b1773a
Anna Neufeld <40243753+anna-neufeld@users.noreply.github.com>
2019-06-05 12:32:01 -0700

    Added other repository

---
+++ final_models.R
@@ -0,0 +1,8 @@
+##### For final models, build:
+### LSA in 2D
+### LSA in more D
+### VBLPCM or LatentNet with 2 Clusters
+### VBLPCM or LatentNet not with 2 clusters
+#### COMPUTE THE PRE/POST TRUMP AVERAGE SCORES FOR EACH
+#### report in table
+### We can see if it changes when we do various cleaning things. 
